9d39270e95a47bcb0107811923d34f66effbcb3b
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2025-05-24 15:17:27 +0200

    leaflet icon

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,14 +4,14 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.scss";
 
-// // Fix for default marker icons in Leaflet with React
-// delete L.Icon.Default.prototype._getIconUrl;
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl:
-//     "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-//   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-//   shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-// });
+// Fix for default marker icons in Leaflet with React
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 // Component to fit map to route bounds
 const FitBounds = ({ bounds }) => {
